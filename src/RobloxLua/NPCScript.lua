0c451fba7ceabebbce07a51b25476a2e131dcc3c
pixol20 <115364463+pixol20@users.noreply.github.com>
2024-03-14 11:41:18 +0200

    update
    change generation settings
    respond every n times
    clear chat log

--- src/RobloxLua/NPCScript.lua
+++ src/RobloxLua/NPCScript.lua
@@ -1,19 +1,32 @@
 local BotName = "Pepe"
 local LogSize = 20
--- local AnswerEverynMessages = 1
--- local NonAnsweredMessages = AnswerEverynMessages
+local AnswerEverynMessages = 1
+local NonAnsweredMessages = AnswerEverynMessages - 1
 local HttpService = game:GetService("HttpService")
 local ChatService = game:GetService("Chat")
 local AppUrl = "YOUR URL HERE"
 Log = {}
 
+function ClearTable(tbl, AmmountToKeep)
+	local keepCount = math.min(AmmountToKeep, #tbl)
+
+	for i = 1, #tbl - keepCount do
+		table.remove(tbl, 1)
+	end
+end
+
 
 function HandleMessage(Player, Message)
 	table.insert(Log, {nickname = Player.name, content = Message})
-	local response = HttpService:PostAsync(AppUrl, HttpService:JSONEncode(Log), Enum.HttpContentType.ApplicationJson, false)
-	game.ReplicatedStorage.ClientSendMsg:FireAllClients(BotName, response)
-	table.insert(Log, {nickname = BotName, content = response})
-	ChatService:Chat(script.Parent.Head, response)
+	NonAnsweredMessages += 1
+	if NonAnsweredMessages >= AnswerEverynMessages then
+		ClearTable(Log, LogSize)
+		NonAnsweredMessages = 0
+		local response = HttpService:PostAsync(AppUrl, HttpService:JSONEncode(Log), Enum.HttpContentType.ApplicationJson, false)
+		game.ReplicatedStorage.ClientSendMsg:FireAllClients(BotName, response)
+		table.insert(Log, {nickname = BotName, content = response})
+		ChatService:Chat(script.Parent.Head, response)
+	end
 end
 
 game.Players.PlayerAdded:Connect(function(Player)
